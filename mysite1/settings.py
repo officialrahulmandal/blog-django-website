4aaf2acaf4d57562878bca70c5d761837b8b368e
Rahul kumar mandal <officialrahulmandal@gmail.com>
2018-03-01 01:13:24 +0530

    done with initial migrations

--- mysite1/settings.py
+++ mysite1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k_%@6j#)#-_#lzydp01a-i=)m=o4t!je=)ij#t$n2^(r#m7x6c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
