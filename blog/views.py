0a6af7c06d67f1e29861e8ad559b792c51d8eda0
Rahul kumar mandal <officialrahulmandal@gmail.com>
2018-03-03 02:54:29 +0530

    extend your application

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import post
 from django.utils import timezone
 # Create your views here.
@@ -9,3 +9,6 @@ def post_list(request):
     posts=post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #posts = post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
+def post_detail(request,pk):
+    past=get_object_or_404(post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':past})
