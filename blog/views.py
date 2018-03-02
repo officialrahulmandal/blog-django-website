b65febf1678395477501d301f6101f70c0fa386f
Rahul kumar mandal <officialrahulmandal@gmail.com>
2018-03-03 03:48:56 +0530

    new form working

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import post
 from .forms import PostForm
 from django.utils import timezone
@@ -14,5 +14,14 @@ def post_detail(request,pk):
     past=get_object_or_404(post,pk=pk)
     return render(request,'blog/post_detail.html',{'post':past})
 def post_new(request):
-    form=PostForm()
+    if request.method=="POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form=PostForm()
     return render(request,'blog/post_edit.html',{'form':form})
